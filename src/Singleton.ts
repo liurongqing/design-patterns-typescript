a6e92a1b1228ea0ab7c7e433c3f57ccb5e71cd4d
liurongqing <phpgege@gmail.com>
2020-10-27 10:12:51 +0800

    1

--- src/Singleton.ts
+++ src/Singleton.ts
@@ -1,13 +1,28 @@
 export default class SingletonInstance {
-    static instance: any
-    constructor(){
-        if(!SingletonInstance.instance) {
-            SingletonInstance.instance = this
+    private instance: any
+    getInstance() {
+        if (!this.instance) {
+            this.instance = new SingletonInstance()
         }
-        return SingletonInstance.instance
-    }
-
-    log(){
-        console.log('123')
+        return this.instance
     }
 }
+
+const singleton = new SingletonInstance()
+const A = singleton.getInstance()
+const B = singleton.getInstance()
+console.log(`A === B： ${A === B}`)
+
+// export default class SingletonInstance {
+//     static instance: any
+//     constructor(){
+//         if(!SingletonInstance.instance) {
+//             SingletonInstance.instance = this
+//         }
+//         return SingletonInstance.instance
+//     }
+
+//     log(){
+//         console.log('123')
+//     }
+// }
